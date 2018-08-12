7c62799bc2924fce5ab3633f4d64098bf71adc84
Bumhan Yu <bumhan.yu@gmail.com>
2018-08-12 15:13:04 -0400

    photo viewer basic function ready

--- js/app.js
+++ js/app.js
@@ -1,33 +1,94 @@
-var photoBtn = document.querySelector('.selection.photos');
-var videoBtn = document.querySelector('.selection.videos');
-var photoList, videoList;
-var fetchList = function(resourceType) {
-  var result = fetch('data/' + resourceType + ".json")
-    .then(function(res) { return res.json()})
-    .then(function(resString) { 
+// 1. Big buttons
+const photoBtn = document.querySelector('.selection.photos');
+const videoBtn = document.querySelector('.selection.videos');
+
+// 2. Lists of media files
+let photoList, videoList = "";
+let flickityString = "";
+
+//
+const photoViewer = document.querySelector('.photoViewer');
+let flkty = "";
+//
+const createFlickityString = fileArray => {
+  let innerDiv = "";
+  for (filename of fileArray) {
+    innerDiv += 
+    `
+      <div class="carousel-cell">
+        <img src="${filename}" />
+      </div>
+    `;
+  };
+  flickityString = `
+      ${innerDiv}
+  `;
+  document.querySelector('.main-carousel').innerHTML = flickityString;
+ 
+  
+}
+
+// 3. Fetching local files
+const fetchList = function(resourceType) {
+  const result = fetch(`data/${resourceType}.json`)
+    .then(res => res.json())
+    .then(resString => { 
       document.querySelector('.preloader').style.display = "none";
       if (resourceType === "images") {
         photoList = Array.from(resString);
+        createFlickityString(photoList);
       } else {
         videoList = Array.from(resString);
-      }
+      };
     })
-    .catch(function(err) { console.log(err)});
+    .catch(err => console.log(err));
   }
 
-var clicked = function(e) {
-  if (e.target === photoBtn) {
-    console.log('photo');
-    console.log(photoList);
-  } else {
-    console.log('video');
-    console.log(videoList);
-  }
+// 4. Photo viewer
+const launchPhotos = () => {
+  photoViewer.style.display = "flex";
+  setTimeout(() => {
+    photoViewer.style.opacity = 1;
+
+  }, 00); 
+
+  // photoViewer.style.opacity = 1;
+  flkty = new Flickity( ".main-carousel", {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    imagesLoaded: true,
+    percentPosition: false,
+    wrapAround: true,
+    autoPlay: 5000
+  });
+    flkty.resize();
 }
-// console.log(photoList);
-window.onload = function() {
+const hidePhotoviewer = () => {
+  photoViewer.style.opacity = 0;
+  setTimeout(() => {
+    photoViewer.style.display = "none";
+
+  }, 500); 
+
+
+}
+// 4. Init
+// - load list and add listners
+window.onload = () => {
+  const clicked = e => {
+    if (e.target === photoBtn) {
+      // console.log('photo');
+      // console.log(photoList);
+      launchPhotos();
+    } else {
+      console.log('video');
+      console.log(videoList);
+    }
+  }
   fetchList("images");
   fetchList("videos");
   photoBtn.addEventListener('click', clicked);
   videoBtn.addEventListener('click', clicked);
+  document.querySelector('.close-photo').addEventListener('click', hidePhotoviewer);
 }
